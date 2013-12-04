090aca868a32ce422719fe4d4c41e1ce843b5bac
Nicole Limtiaco <limni@seas.upenn.edu>
2013-12-03 19:38:38 -0500

    added ratings and rate and pin buttons

--- Pennterest/routes/home.js
+++ Pennterest/routes/home.js
@@ -18,37 +18,37 @@ function getPins_db(res, id) {
 	    } else {
 	    	//user's first 5 pins
 	    	var userPins = "(SELECT * FROM " +
-	    			"(SELECT CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION," +
-	    			"  LISTAGG(TAG, ' ') WITHIN GROUP (ORDER BY TAG) as tags FROM " +
-		    "(SELECT c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG " +
-		    "FROM PIN p, CONTENT c, USERS u, CONTENTTAG ct, TAG t " +
+	    			"(SELECT CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION, AVG(RATING) as RATING," +
+	    			"  LISTAGG(TAG, ' #') WITHIN GROUP (ORDER BY TAG) as tags FROM " +
+		    "(SELECT c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG, pr.RATING " +
+		    "FROM PIN p, CONTENT c, USERS u, CONTENTTAG ct, TAG t, PINRATING pr " +
 		    "WHERE p.USERID=" +id+ " AND p.CONTENTID = c.CONTENTID AND u.USERID=" +id +
-		    " AND ct.CONTENTID = c.CONTENTID AND t.TAGID = ct.TAGID)" +
+		    " AND ct.CONTENTID = c.CONTENTID AND t.TAGID = ct.TAGID AND pr.PINID = p.PINID)" +
 		    " GROUP BY PINID, CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION ORDER BY PINID)" +
 		    " WHERE ROWNUM <= 5)";
 	    	console.log(userPins);
 	    	//first 10 pins tagged with user's interests
 	    	var interestsPins = "(SELECT * FROM " +
-	    			"(SELECT CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION," +
-	    			" LISTAGG(TAG, ' ') WITHIN GROUP (ORDER BY TAG) as tags FROM " +
-	    			"(SELECT c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG " +
-	    			"FROM PIN p, CONTENTTAG ct1, CONTENTTAG ct2, TAG t, INTERESTED i, USERS u, CONTENT c " +
+	    			"(SELECT CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION, AVG(RATING) as RATING," +
+	    			" LISTAGG(TAG, ' #') WITHIN GROUP (ORDER BY TAG) as tags FROM " +
+	    			"(SELECT c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG, pr.RATING " +
+	    			"FROM PIN p, CONTENTTAG ct1, CONTENTTAG ct2, TAG t, INTERESTED i, USERS u, CONTENT c, PINRATING pr " +
 	    			"WHERE p.CONTENTID = ct1.CONTENTID AND i.TAGID = ct1.TAGID AND i.USERID =" + id +
 	    					" AND p.CONTENTID = c.CONTENTID AND u.USERID = p.USERID" +
-	    					" AND ct2.CONTENTID = p.CONTENTID AND t.TAGID = ct2.TAGID" +
+	    					" AND ct2.CONTENTID = p.CONTENTID AND t.TAGID = ct2.TAGID AND pr.PINID = p.PINID" +
 	    					" ORDER BY p.PINID) " +
 					" GROUP BY PINID, CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION ORDER BY PINID) " +
 					" WHERE ROWNUM <= 10)";
 	    	console.log(interestsPins);
 	    	//first 10 pins of people the user is following
 	    	var followedsPins = "(SELECT * FROM " +
-	    			"(SELECT CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION," +
+	    			"(SELECT CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION, AVG(RATING) as RATING," +
 	    			"  LISTAGG(TAG, ' #') WITHIN GROUP (ORDER BY TAG) as tags FROM " +
-	    			"(SELECT c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG " +
-	    			"FROM PIN p, CONTENT c, USERS u, FOLLOWING f, CONTENTTAG ct, TAG t " +
+	    			"(SELECT c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG, pr.RATING " +
+	    			"FROM PIN p, CONTENT c, USERS u, FOLLOWING f, CONTENTTAG ct, TAG t, PINRATING pr " +
 	    			"WHERE f.FOLLOWER = " +id + " AND p.USERID = f.FOLLOWED " +
 	    			"AND p.CONTENTID = c.CONTENTID AND u.USERID = f.FOLLOWED " +
-	    			"AND ct.CONTENTID = c.CONTENTID AND ct.TAGID = t.TAGID) " +
+	    			"AND ct.CONTENTID = c.CONTENTID AND ct.TAGID = t.TAGID AND pr.PINID = p.PINID) " +
 	    			" GROUP BY PINID, CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION ORDER BY PINID)" +
 	    			" WHERE ROWNUM <= 10) ";
 	    	console.log(followedsPins);
