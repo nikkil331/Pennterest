f1c7bdfa410b48da9e10e238c7177d5b2faa1347
Nicole Limtiaco <limni@seas.upenn.edu>
2013-12-05 20:40:23 -0500

    pin functionality working! parses description for tags displays user description or tags if unavailable

--- Pennterest/routes/home.js
+++ Pennterest/routes/home.js
@@ -25,7 +25,7 @@ function getPins_db(res, id) {
 		    "(SELECT c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG, AVG(pr.RATING) as RATING " +
 		    "FROM PIN p, CONTENT c, USERS u, CONTENTTAG ct, TAG t, PINRATING pr " +
 		    "WHERE p.USERID=" +id+ " AND p.CONTENTID = c.CONTENTID AND u.USERID=" +id +
-		    " AND ct.CONTENTID = c.CONTENTID AND t.TAGID = ct.TAGID AND pr.PINID = p.PINID" +
+		    " AND c.CONTENTID = ct.CONTENTID(+) AND ct.TAGID = t.TAGID(+) AND p.PINID = pr.PINID(+)" +
 		    " GROUP BY c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG)" +
 		    " GROUP BY PINID, CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION, RATING ORDER BY PINID)" +
 		    " WHERE ROWNUM <= 5)";
@@ -38,7 +38,7 @@ function getPins_db(res, id) {
 	    			"FROM PIN p, CONTENTTAG ct1, CONTENTTAG ct2, TAG t, INTERESTED i, USERS u, CONTENT c, PINRATING pr " +
 	    			"WHERE p.CONTENTID = ct1.CONTENTID AND i.TAGID = ct1.TAGID AND i.USERID =" + id +
 	    					" AND p.CONTENTID = c.CONTENTID AND u.USERID = p.USERID" +
-	    					" AND ct2.CONTENTID = p.CONTENTID AND t.TAGID = ct2.TAGID AND pr.PINID = p.PINID" +
+	    					" AND p.CONTENTID = ct2.CONTENTID(+) AND ct2.TAGID = t.TAGID(+) AND p.PINID = pr.PINID(+)" +
 	    					" GROUP BY c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG" +
 	    					" ORDER BY p.PINID) " +
 					" GROUP BY PINID, CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION, RATING ORDER BY PINID) " +
@@ -52,7 +52,7 @@ function getPins_db(res, id) {
 	    			"FROM PIN p, CONTENT c, USERS u, FOLLOWING f, CONTENTTAG ct, TAG t, PINRATING pr " +
 	    			"WHERE f.FOLLOWER = " +id + " AND p.USERID = f.FOLLOWED " +
 	    			"AND p.CONTENTID = c.CONTENTID AND u.USERID = f.FOLLOWED " +
-	    			"AND ct.CONTENTID = c.CONTENTID AND ct.TAGID = t.TAGID AND pr.PINID = p.PINID" +
+	    			"AND c.CONTENTID = ct.CONTENTID(+) AND ct.TAGID = t.TAGID(+) AND p.PINID = pr.PINID(+)" +
 	    			" GROUP BY c.CONTENTPATH, u.FIRSTNAME, p.BOARDNAME, p.CAPTION, p.PINID, t.TAG) " +
 	    			" GROUP BY PINID, CONTENTPATH, FIRSTNAME, BOARDNAME, CAPTION, RATING ORDER BY PINID)" +
 	    			" WHERE ROWNUM <= 10) ";
@@ -89,7 +89,7 @@ function getPins_db(res, id) {
 
 
 exports.home = function(req, res){
-  getPins_db(res, 1);
+  getPins_db(res, 109);
 };
 
 //adds new rating
@@ -134,36 +134,131 @@ exports.update = function(req, res){
 
 //adds new pin
 exports.addPin = function(req, res){
+	var description = req.body.description;
+	tags = getTags(description);
+	console.log(tags);
+	var contentid;
 	oracle.connect(connectData, function(err, connection) {
 	    if ( err ) {
-	    	console.log(err + " first error");
+	    	console.log(err);
 	    } else {
 	    	var query = "SELECT CONTENTID FROM PIN WHERE PINID = " + req.body.pinID;
 	    	console.log(query);
 	    	connection.execute(query,
-	    			[],
-	    			function(err, cid){
+	    		[],
+	    		function(err, results){
 	    			if(err) {console.log(err);}
 	    			else{
-	    				console.log(cid);
-	    				query = "INSERT INTO PIN (USERID, CONTENTID, BOARDNAME, CAPTION) VALUES" +
-		    			" (" + req.body.userID + ", " + cid[0]["CONTENTID"] + ", \'" + req.body.boardName + 
-		    			"\', '" + req.body.description + "')";
-	    				console.log(query);
-	    				connection.execute(query,
-	    						[],
-	    						function(err, results){
-	    							if(err) {console.log(err);}
-	    							else{
-	    								connection.close();
-	    								res.end();
-	    							}
-	    						});
-	    				}
-	    			});
+	    				console.log("now to pin...");
+	    				var data = {"contentid":results[0]["CONTENTID"], "userid":req.body.userID, "boardname":req.body.boardName, 
+	    						"description":req.body.description, "tags":tags, "connection":connection, "response":res};
+	    				pinContent(data);
+	    			}
+	    		});
 	    	}
+		});
+}
+
+function getTags(description){
+	var alphanumeric = new RegExp('[0-9a-zA-z]');
+	var tags = [];
+	for(var i = 0; i < description.length; i++){
+		var c = description.charAt(i);
+		if(c == "#"){
+			var tag = "";
+			i++;
+			c = description.charAt(i);
+			while(alphanumeric.test(c)){
+				tag = tag +  c;
+				i++;
+				c = description.charAt(i);
+			}
+			tags.push(tag);
+			i--;
+		}
+	}
+	return tags;
+}
+
+function pinContent(data){
+	var query = "INSERT INTO PIN (USERID, CONTENTID, BOARDNAME, CAPTION, PINID) VALUES" +
+	" (" + data["userid"] + ", " + data["contentid"] + ", \'" + data["boardname"] + 
+	"\', '" + data["description"] + "' " + ", seq_pin_id.nextval)";
+	console.log(query);
+	data["connection"].execute(query, [], 
+		function(err, results){
+			if(err) { console.log(err); }
+			else{
+				if(data["tags"].length > 0){
+					console.log("tags.length > 0");
+					addTags(data);
+				}
+			}
+		});
+}
+
+function addTags (data){
+	console.log('adding tags...');
+	for(var i = 0; i < data["tags"].length; i++){
+		getTagId(data, i);
+	}
+}
+
+function getTagId(data, index){
+	console.log('getting tag ids...');
+	query = "SELECT TAGID FROM TAG WHERE TAG='" + data["tags"][index] + "'";
+	console.log(query);
+	data["connection"].execute(query, [], 
+	function(err, results){
+		if(err) {console.log(err);}
+		if(results.length > 1){
+			insertContentTag(data, results[0]["TAGID"]);
+		}
+		else{
+			insertTag(data, data["tags"][index]);
+		}
 	});
 }
+
+function insertContentTag (data, tagid){
+	console.log('inserting content tag');
+	query = "INSERT INTO CONTENTTAG (TAGID, CONTENTID) VALUES (" + tagid +
+			", " + data["contentid"] + ")";
+	console.log(query);
+	data["connection"].execute(query,
+			[],
+	function(err,insertRes){
+		if(err) {console.log(err);}
+		data["connection"].close();
+		data["response"].end();
+	});
+}
+
+function insertTag(data, tag){
+	query = "INSERT INTO TAG (TAGID, TAG) VALUES (seq_tag_id.nextval, '" +
+	tag + "')";
+	console.log(query);
+	data["connection"].execute(query,
+	[],
+	function(err, insertRes){
+		if(err) {console.log(err);}
+		else{
+			query = "INSERT INTO CONTENTTAG (TAGID, CONTENTID) VALUES (" + 
+			"(SELECT seq_tag_id.currval FROM dual), " + data["contentid"];
+			console.log(query);
+			data["connection"].execute(query,
+			[],
+			function(err, results){
+				if(err) { console.log(err); }
+				data["connection"].close();
+	    		data["response"].end();
+			});
+		}
+	});
+}
+
+
+
 
 
 
