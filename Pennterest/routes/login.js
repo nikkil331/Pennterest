add521b30cd32b58fa3f758872372ea9ab8f4efa
Eric O'Brien <3ricob@gmail.com>
2013-12-08 23:09:25 -0500

    hope this works

---
+++ Pennterest/routes/login.js
@@ -0,0 +1,18 @@
+var connectData = {
+		"hostname": "cis550zne.cbh8gmdnynf7.us-east-1.rds.amazonaws.com",
+		"user": "zne",
+		"password": "jacksonf",
+		"database": "PENNZNE"
+	};
+
+var oracle =  require("oracle");
+
+
+exports.login = function(req, res){
+	if (true /*usr logged in*/) {
+		res.redirect('/home');
+	}
+	else {
+		res.render('login', { });
+	}
+};
